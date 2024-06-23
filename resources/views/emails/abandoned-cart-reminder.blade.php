ec03306fbf148a89e1bf3d7ba6b3ad1a29b501eb
Master Muntazir <me@muntazir.in>
2024-06-23 18:50:12 +0530

    test commit

--- resources/views/emails/abandoned-cart-reminder.blade.php
+++ resources/views/emails/abandoned-cart-reminder.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 Hey {{ $cart->user->name }},
 
-You still have items in your cart. Click the button below to continue your checkout.
+You still have items in your cart. Click the button below if you want to continue continue your checkout.
 
 @component('mail::button', ['url' => route('cart'), 'color' => 'success'])
 Continue Checkout
